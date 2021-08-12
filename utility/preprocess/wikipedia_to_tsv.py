49d7fb69bdb2d7340780bad2926b06f6ce14d928
Omar Khattab <okhat@users.noreply.github.com>
2021-08-11 20:32:08 -0700

    Update wikipedia_to_tsv.py

--- utility/preprocess/wikipedia_to_tsv.py
+++ utility/preprocess/wikipedia_to_tsv.py
@@ -29,19 +29,24 @@ def main(args):
                     RawCollection.append(ujson.loads(line))
 
     with open(output_path, 'w') as f:
-        line = '\t'.join(map(str, ['id', 'text', 'title'])) + '\n'
-        f.write(line)
+        #line = '\t'.join(map(str, ['id', 'text', 'title'])) + '\n'
+        #f.write(line)
 
-        for pid_, doc in enumerate(RawCollection):
-            pid = pid_ + 1
+        PID = 1
+
+        for doc in RawCollection:
             title, text = doc['title'], doc['text']
 
             # Join sentences and clean text
             text = ' '.join(text.split())
 
             if args.keep_empty_pages or len(text) > 0:
-                line = '\t'.join(map(str, [pid, text, title])) + '\n'
+                line = '\t'.join(map(str, [PID, text, title])) + '\n'
                 f.write(line)
+
+                PID += 1
+    
+    print_message("#> All done.")
 
 
 if __name__ == "__main__":
