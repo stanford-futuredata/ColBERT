c1905c67341cc4de879471f4ca8fecdb85443904
Omar Khattab <okhat@users.noreply.github.com>
2021-11-10 16:23:18 -0800

    Update amp.py

--- colbert/utils/amp.py
+++ colbert/utils/amp.py
@@ -3,7 +3,7 @@ import torch
 from contextlib import contextmanager
 from colbert.utils.utils import NullContextManager
 
-PyTorch_over_1_6 = float('.'.join(torch.__version__.split('.')[0:2])) >= 1.6
+PyTorch_over_1_6 = True # float('.'.join(torch.__version__.split('.')[0:2])) >= 1.6
 
 
 class MixedPrecisionManager():
