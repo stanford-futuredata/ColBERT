af543e22bf174d9d82598b7c5e19515c3f42a9d2
xm-gui <xg64@cornell.edu>
2022-07-04 13:58:54 -0700

    use squad for evaluation

--- e2e_test.py
+++ e2e_test.py
@@ -1,20 +1,21 @@
 import os
-from collections import defaultdict
-import jsonlines
-import sys
+from collections import namedtuple
+from datasets import load_dataset
+from utility.utils.dpr import has_answer, DPR_normalize
 
 from colbert import Indexer, Searcher
 from colbert.infra import ColBERTConfig, RunConfig, Run
-from colbert.data import Queries
 
 CKPT = "/dfs/scratch0/okhattab/share/2021/checkpoints/msmarco.psg.kldR2.nway64.ib__colbert-400000"
-STACKEXCHANGE_DATA_PATH = "/future/u/xmgui/testdata"
+DATA_PATH = "/future/u/xmgui/testdata"
 EXPERIMENT_DIR = "/future/u/xmgui/ColBERT/experiments"
+
+SquadExample = namedtuple("SquadExample",  "id title context question answers")
 
 
 def build_index(dataset_path):
-    nbits = 2  # encode each dimension with 2 bits
-    doc_maxlen = 300  # truncate passages at 300 tokens
+    nbits = 1  # encode each dimension with 1 bits
+    doc_maxlen = 180  # truncate passages at 180 tokens
     collection = os.path.join(dataset_path, 'cs224u.collection.tsv')
     index_name = f'e2etest.{nbits}bits.latest'
     experiment = (f"e2etest.nbits={nbits}",)
@@ -41,59 +42,38 @@ def build_index(dataset_path):
         return searcher
 
 
-def search(searcher, dataset_path, query_type, k):
-    queries = os.path.join(
-        dataset_path, f"questions.{query_type}.tsv"
-    )
-    queries = Queries(path=queries)
-    ranking = searcher.search_all(queries, k=k)
-    output_path = ranking.save(f"e2etest.{query_type}.ranking.tsv")
-    return output_path
+def success_at_k(searcher, examples, k):
+    scores = []
+    for ex in examples:
+        scores.append(evaluate_retrieval_example(searcher, ex, k))
+    return sum(scores) / len(scores)
 
 
-def evaluate_dataset(dataset_path, query_type, k, rankings_path):
-    if not os.path.exists(rankings_path):
-        print(f"[query_type={query_type}] Success@{k}: ???")
-        return
-    rankings = defaultdict(list)
-    with open(rankings_path, "r") as f:
-        for line in f:
-            items = line.strip().split("\t")
-            qid, pid, rank = items[:3]
+def evaluate_retrieval_example(searcher, ex, k):
+    results = searcher.search(ex.question, k=k)
+    for passage_id, passage_rank, passage_score in zip(*results):
+        passage = searcher.collection[passage_id]
+        score = has_answer([DPR_normalize(ans) for ans in ex.answers], passage)
+        if score:
+            return 1
+    return 0
 
-            qid = int(qid)
-            pid = int(pid)  # post id
-            rank = int(rank)
-            rankings[qid].append(pid)
-            assert rank == len(rankings[qid])
 
-    success = 0
-    qas_path = os.path.join(dataset_path, f"qas.{query_type}.jsonl")
-
-    # check if the generated ranking's top k intersects with the provided answer
-    num_total_qids = 0
-    with jsonlines.open(qas_path, mode="r") as f:
-        for line in f:
-            qid = int(line["qid"])
-            num_total_qids += 1
-            if qid not in rankings:
-                print(f"WARNING: qid {qid} not found in {rankings_path}!", file=sys.stderr)
-                continue
-            answer_pids = set(line["answer_pids"])
-            if len(set(rankings[qid][:k]).intersection(answer_pids)) > 0:
-                success += 1
-    print(
-        f"[query_type={query_type}] "
-        f"Success@{k}: {success / num_total_qids * 100:.1f}"
-    )
+def get_squad_split(squad, split="validation"):
+    fields = squad[split].features
+    data = zip(*[squad[split][field] for field in fields])
+    return [SquadExample(eid, title, context, question, answers["text"])
+            for eid, title, context, question, answers in data]
 
 
 def main():
     k = 5
-    searcher = build_index(STACKEXCHANGE_DATA_PATH)
-    for query_type in ["search", "forum"]:
-        rankings_path = search(searcher, STACKEXCHANGE_DATA_PATH, query_type, k)
-        evaluate_dataset(STACKEXCHANGE_DATA_PATH, query_type, k, rankings_path)
+    searcher = build_index(DATA_PATH)
+
+    squad = load_dataset("squad")
+    squad_dev = get_squad_split(squad)
+    success_rate = success_at_k(searcher, squad_dev, k)
+    assert success_rate > 0.7, f"success rate at {success_rate} is lower than expected"
 
 
 if __name__ == "__main__":
