ca547249c19320104b3c671626cdf532c415486b
Uma Dingankar <udingank@stanford.edu>
2023-01-26 17:51:25 -0800

    cleanup mmap test
    Added axes labels
    Changed to GB
    Copy from disk explicit
    Sum over copy of buffer chunk instead of addition
    Report memory usage from worker process

--- colbert/tests/mmap_test.py
+++ colbert/tests/mmap_test.py
@@ -1,9 +1,10 @@
 import argparse
+import copy
 import gc
 import matplotlib.pyplot
 import random
 import os, psutil
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 import sys
 import time
 import torch
@@ -15,18 +16,24 @@ TEST_FILE_SIZE = 1209306560
 TEST_CHUNK_SIZE = 100000
 BYTE_SIZE = 1
 
-NUM_READ_CYCLES = 10
-NUM_RAND_CYCLES = 1000
+NUM_COMPUTE_CYCLES = 100
 
 # wait 10 msec to poll
 SAMPLE_PERIOD_SEC = 0.01
 
 RESULTS_FILENAME = 'results.png'
 
+B_PER_GB = 1e9
 
-def read_into_buffer(mmap, read_buf_size, read_rand):
-    # create memory buffer, read into it NUM_READ_CYCLES times
+control_basic = []
+mmap_basic = []
+control_compute = []
+mmap_compute = []
+
+def read_into_buffer(mmap, read_buf_size, q):
+
     print("Starting worker process {}".format(os.getpid()))
+    proc = psutil.Process(os.getpid())
 
     start_time = time.time()
 
@@ -35,37 +42,58 @@ def read_into_buffer(mmap, read_buf_size, read_rand):
 
     if mmap:
         mem_buf = torch.ByteTensor(storage)
+        mem = proc.memory_info().rss/B_PER_GB
+        q.put(mem)
     else:
         mem_buf = torch.load(TestFile, map_location='cpu')
+        mem = proc.memory_info().rss/B_PER_GB
+        q.put(mem)
 
     fetch_time = time.time()
     print("it took {}s to read from disk".format(fetch_time - start_time))
 
-    if read_rand:
-        copy_avg = 0
-        calc_avg = 0
-        for i in range(NUM_RAND_CYCLES):
-            rand_time_start = time.time()
-            start = random.randrange(0, TEST_FILE_SIZE)
-            end = max(start + TEST_CHUNK_SIZE, TEST_FILE_SIZE)
-            new_buf = mem_buf[start:end]
-            ts1 = time.time()
-            new_buf = torch.add(new_buf, 1)
-            ts2 = time.time()
-            copy_avg = copy_avg + ts1 - rand_time_start
-            calc_avg = calc_avg + ts2 - ts1
-            del new_buf
-            gc.collect()
+    if mmap:
+        results_buf = mmap_compute
+    else:
+        results_buf = control_compute
 
-        copy_avg = copy_avg/NUM_RAND_CYCLES
-        calc_avg = calc_avg/NUM_RAND_CYCLES
-        print("copy average time = {}, calculation average time = {}".format(copy_avg, calc_avg))
+    copy_avg = 0
+    calc_avg = 0
+    results_buf = []
+
+    for i in range(NUM_COMPUTE_CYCLES):
+        if (i % 10 == 0):
+            print("copying cycle {}".format(i))
+
+        rand_time_start = time.time()
+        start = random.randrange(0, TEST_FILE_SIZE)
+        end = max(start + TEST_CHUNK_SIZE, TEST_FILE_SIZE)
+        new_buf = copy.deepcopy(mem_buf[start:end])
+        ts1 = time.time()
+
+        computation = torch.sum(new_buf)
+        ts2 = time.time()
+
+        copy_avg = copy_avg + ts1 - rand_time_start
+        calc_avg = calc_avg + ts2 - ts1
+
+        del new_buf
+        gc.collect()
+
+        mem = proc.memory_info().rss
+        results_buf.append(mem/B_PER_GB)
+
+    copy_avg = copy_avg/NUM_COMPUTE_CYCLES
+    calc_avg = calc_avg/NUM_COMPUTE_CYCLES
+    print("copy average time = {}, calculation average time = {}".format(copy_avg, calc_avg))
 
     del mem_buf
     gc.collect()
 
     end_time = time.time()
     print("fetch to end time = {}".format(end_time - fetch_time))
+
+    q.put(results_buf)
 
     return
 
@@ -86,34 +114,27 @@ def read_into_buffer(mmap, read_buf_size, read_rand):
 # The parent process will monitor the memory usage of the worker
 # process for each test iteration and report back the results to
 # the caller for analysis.
-def run_test(mmap, test_iter, read_buf_size, read_rand):
-    results = []
-
+def run_test(mmap, test_iter, read_buf_size):
     for i in range(test_iter):
         print("Starting iteration {}".format(i))
 
-        start_time = time.time()
-        # Spawn a process and track the worker's PID
-        worker_proc = Process(target=read_into_buffer, args=(mmap, read_buf_size, read_rand))
-        worker_proc.start()
-        worker_pid = worker_proc.pid
+        q = Queue()
 
-        # While the worker is not finished, periodically track memory usage
-        cur_result = []
-        worker_mem_proc = psutil.Process(worker_pid)
-        while worker_proc.is_alive():
-            mem = worker_mem_proc.memory_info().rss
-            cur_result.append(mem)
-            time.sleep(SAMPLE_PERIOD_SEC)
+        start_time = time.time()
+        worker_proc = Process(target=read_into_buffer, args=(mmap, read_buf_size, q))
+        worker_proc.start()
 
         worker_proc.join()
         end_time = time.time()
 
-        results.extend(cur_result)
         print("exec time = {}".format(end_time - start_time))
 
-    return results
-
+        if mmap:
+            mmap_basic.append(q.get())
+            mmap_compute.extend(q.get())
+        else:
+            control_basic.append(q.get())
+            control_compute.extend(q.get())
 
 def main(args):
     target_dir = args.target_dir
@@ -124,19 +145,12 @@ def main(args):
     ### Start the test
     print("\nStarting MMap stress test")
 
-    ### Run the basic mem test with + without MMap, save memory usage stats
-    print("\nStarting basic control test")
-    control_basic = run_test(False, test_iter, read_buf_size, False)
-
-    print("\nStarting basic mmap test")
-    mmap_basic = run_test(True, test_iter, read_buf_size, False)
-
     ### Read random parts of the test data file with and without MMap
     print("\nStarting random file read control test")
-    control_rand = run_test(False, test_iter, read_buf_size, True)
+    run_test(False, test_iter, read_buf_size)
 
     print("\nStarting random file read mmap test")
-    mmap_rand = run_test(True, test_iter, read_buf_size, True)
+    run_test(True, test_iter, read_buf_size)
 
     ### Cleanup
     print("\nFinished MMap stress test\n")
@@ -146,14 +160,14 @@ def main(args):
     #   files and not, in terms of memory pressure
 
     print("Saving Results\n--------------")
-    results = [control_basic, mmap_basic, control_rand, mmap_rand]
-    labels = ['control_basic', 'mmap_basic', 'control_rand', 'mmap_rand']
+    results = [control_basic, mmap_basic, control_compute, mmap_compute]
+    labels = ['control_basic', 'mmap_basic', 'control_compute', 'mmap_compute']
 
     fig = matplotlib.pyplot.figure()
     plot = fig.add_subplot(111)
 
-    plot.boxplot(results, labels=labels)
-    plot.set_ylabel('Memory Usage (B)')
+    plot.boxplot(results, labels=labels, showmeans=True)
+    plot.set_ylabel('Memory Usage (GB)')
 
     result_filename = os.path.join(target_dir, RESULTS_FILENAME)
     matplotlib.pyplot.savefig(result_filename)
@@ -171,7 +185,7 @@ if __name__ == "__main__":
 
     # optional arguments
     parser.add_argument(
-        "--test_iter", type=int, required=False, default=3, help="Number of test cycles"
+        "--test_iter", type=int, required=False, default=1, help="Number of test cycles"
     )
     parser.add_argument(
         "--read_buf_size", type=int, required=False, default=TEST_FILE_SIZE,\
