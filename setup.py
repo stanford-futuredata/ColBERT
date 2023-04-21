e5ebff53524a1cc9a0e3d9d5ba2af48b8077ef35
Isaiah Leonard <isaiah.leonard@c3.ai>
2023-04-20 21:57:18 -0600

    Create MANIFEST.in Add include_package_data=True to setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/stanford-futuredata/ColBERT',
     packages=setuptools.find_packages(),
+    include_package_data=True,
     python_requires='>=3.6',
 )
