8f700e9f2672a6b1d565d16d04ef962654389a0e
Thejas <thejas@Thejass-MacBook-Pro-3.local>
2023-09-04 19:17:19 +0530

    Return results

--- eval_server.py
+++ eval_server.py
@@ -1,31 +1,26 @@
 import grpc
-import asyncio
 from concurrent import futures
 import torch
 import server_pb2
 import server_pb2_grpc
 
 import os
-import sys
 import gc
 
-from threading import Lock
-import math
 import time
-import csv 
+import csv
 
-from colbert.infra import Run, RunConfig, ColBERTConfig
 from colbert import Searcher
 from collections import defaultdict
+
 
 class ColBERTServer(server_pb2_grpc.ServerServicer):
     def __init__(self):
         k = 100
         self.index_name = "msmarco.nbits=2.latest.mmap"
-        experiment = (self.index_name,)
         gold_rankings_files = "/data/msmarco.k=1000.device=gpu.ranking.tsv"
         self.gold_ranks = defaultdict(list)
-        
+
         with open(gold_rankings_files, newline='', encoding='utf-8') as tsvfile:
             tsv_reader = csv.reader(tsvfile, delimiter='\t')
             for line in tsv_reader:
@@ -33,7 +28,6 @@ class ColBERTServer(server_pb2_grpc.ServerServicer):
 
         self.searcher = Searcher(index=f"/data/indexes/{self.index_name}/")
         self.ranker = self.searcher.ranker
-
 
     def convert_dict_to_protobuf(self, input_dict):
         query_result = server_pb2.QueryResult()
@@ -47,23 +41,23 @@ class ColBERTServer(server_pb2_grpc.ServerServicer):
             topk_result.score = topk_dict["score"]
 
         return query_result
-    
+
     def api_search_query(self, query, qid, k=100):
         gc.collect()
         t2 = time.time()
-        pids, ranks, scores = self.searcher.search(query, k=100) 
+        pids, ranks, scores = self.searcher.search(query, k)
         print("Searching time of {}: {}".format(qid, time.time() - t2))
+
+        top_k = []
+        for pid, rank, score in zip(pids, ranks, scores):
+            top_k.append({'pid': pid, 'rank': rank, 'score': score})
+        top_k = list(sorted(top_k, key=lambda p: (-1 * p['score'], p['pid'])))
+
         del pids
         del ranks
         del scores
 
-        return server_pb2.QueryResult()
-        # topk = []
-        # for pid, rank, score in zip(pids, ranks, scores):
-        #    d = {'pid': pid, 'rank': rank, 'score': score}
-        #    topk.append(d)
-        # topk = list(sorted(topk, key=lambda p: (-1 * p['score'], p['pid'])))
-        # return self.convert_dict_to_protobuf({"query" : query, "topk": topk})
+        return self.convert_dict_to_protobuf({"query": query, "topk": top_k})
 
     def api_rerank_query(self, query, qid, k=100):
         gc.collect()
@@ -72,17 +66,20 @@ class ColBERTServer(server_pb2_grpc.ServerServicer):
         Q = self.searcher.encode([query])
         # score = ranker.score_raw_pids(config, Q[i:i+1], torch.tensor(list(docs)[:10000], dtype=torch.int))
         scores_, pids_ = self.ranker.score_raw_pids(self.searcher.config, Q, gr)
-        scores_sorter = scores_.sort(descending=True)
-        pids_, scores_ = pids_[scores_sorter.indices].tolist(), scores_sorter.values.tolist()
         print("Reranking time of {}: {}".format(qid, time.time() - t2))
         # times.append(time.time() - t2)
+
+        top_k = []
+        for pid, rank, score in zip(pids_, range(len(pids_)), scores_):
+            top_k.append({'pid': pid, 'rank': rank, 'score': score})
+        top_k = list(sorted(top_k, key=lambda p: (-1 * p['score'], p['pid'])))
+
         del gr
         del scores_
         del pids_
-        del scores_sorter
         del Q
 
-        return server_pb2.QueryResult(query=query)
+        return self.convert_dict_to_protobuf({"query": query, "topk": top_k})
 
     def Search(self, request, context):
         torch.set_num_threads(1)
@@ -94,6 +91,7 @@ class ColBERTServer(server_pb2_grpc.ServerServicer):
         torch.set_num_threads(1)
         return self.api_rerank_query(request.query, request.qid, request.k)
 
+
 def serve_ColBERT_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     server_pb2_grpc.add_ServerServicer_to_server(ColBERTServer(), server)
@@ -104,5 +102,6 @@ def serve_ColBERT_server():
     server.wait_for_termination()
     print("Terminated")
 
+
 if __name__ == '__main__':
-   serve_ColBERT_server()
+    serve_ColBERT_server()
