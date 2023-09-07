ddcfdaf8322843972f355765c6b9d358fc6befd4
santhnm2 <keshav2@stanford.edu>
2023-09-07 13:08:39 -0700

    Copy plan.json

--- colbert/utils/coalesce.py
+++ colbert/utils/coalesce.py
@@ -77,19 +77,27 @@ def main(args):
         ujson.dump(metadata, f, indent=4)
 
     metadata_0 = {}
-    metadata_0["num_embeddings"] = metadata["num_embeddings"] 
-    metadata_0["passage_offset"] = 0 
+    metadata_0["num_embeddings"] = metadata["num_embeddings"]
+    metadata_0["passage_offset"] = 0
     metadata_0["embedding_offset"] = 0
-    
+
     filepath = os.path.join(in_file, str(num_chunks-1) + '.metadata.json')
     with open(filepath, 'r') as f:
-        metadata_last = ujson.load(f) 
+        metadata_last = ujson.load(f)
         metadata_0["num_passages"] = int(metadata_last["num_passages"]) + int(metadata_last["passage_offset"])
-    
+
     filepath = os.path.join(out_file, '0.metadata.json')
     with open(filepath, 'w') as f:
         ujson.dump(metadata_0, f, indent=4)
-    
+
+    filepath = os.path.join(in_file, 'plan.json')
+    with open(filepath, 'r') as f:
+        plan = ujson.load(f)
+    plan['num_chunks'] = 1
+    filepath = os.path.join(out_file, 'plan.json')
+    with open(filepath, 'w') as f:
+        ujson.dump(plan, f, indent=4)
+
     other_files = ['avg_residual.pt', 'buckets.pt', 'centroids.pt', 'ivf.pt', 'ivf.pid.pt']
     for filename in other_files:
         filepath = os.path.join(in_file, filename)
