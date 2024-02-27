2e51236736d8c01cff3cf36c8815d39cc59c9351
Josh Purtell <jmvpurtell@gmail.com>
2024-02-27 11:16:52 -0500

    add logic that only tries to parallelize when there are gpus across which to parallelize

--- colbert/training/training.py
+++ colbert/training/training.py
@@ -52,12 +52,10 @@ def train(config: ColBERTConfig, triples, queries=None, collection=None):
     colbert = colbert.to(DEVICE)
     colbert.train()
 
-    try:
+    if torch.cuda.device_count() > 1:
         colbert = torch.nn.parallel.DistributedDataParallel(colbert, device_ids=[config.rank],
                                                             output_device=config.rank,
                                                             find_unused_parameters=True)
-    except:
-        pass
 
     optimizer = AdamW(filter(lambda p: p.requires_grad, colbert.parameters()), lr=config.lr, eps=1e-8)
     optimizer.zero_grad()
